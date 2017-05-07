7253523a694dbceea5abf4e0d44e5b7d588977a7
se-vl <se-vl@users.noreply.github.com>
2017-05-07 17:49:39 +0200

    put random quote in quotation marks

--- quotes/src/servlets/QuoteServlet.java
+++ quotes/src/servlets/QuoteServlet.java
@@ -35,7 +35,7 @@ public class QuoteServlet extends HttpServlet {
 		int randomIndex = randomNumberGenerator.nextInt(numberOfQuotes);
 		String randomQuote = wordsOfWisdom[randomIndex];
 
-		out.println(randomQuote);
+		out.println("\"" + randomQuote + "\"");
 		out.println("</p></body></html>");
 	}
 
