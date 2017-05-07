fc12cceb901380294988c7b8567b7fee81485b57
se-vl <se-vl@users.noreply.github.com>
2017-05-07 17:43:36 +0200

    pick first quote

--- quotes/src/servlets/QuoteServlet.java
+++ quotes/src/servlets/QuoteServlet.java
@@ -13,13 +13,23 @@ import javax.servlet.http.HttpServletResponse;
 public class QuoteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	private static final String[] wordsOfWisdom = {
+			"Multicore processors are just now [May 2006] becoming inexpensive enough for midrange desktop systems. Not coincidentally, many development teams are noticing more and more threading-related bug reports in their projects.",
+			"In a recent post on the Netbeans developer site, one of the core maintainers observed that a single class had been patched over 14 times to fix threading-related problems.",
+			"Dion Almaer, former editor of TheServerSide, recently blogged (after a painful debugging session that ultimately revealed a threading bug) that most Java programs are so rife with concurrency bugs that they work only 'by accident'.",
+			"It is all too easy to create programs that compile and appear to work but are nevertheless broken.",
+			"It would be nice to believe that concurrency is an 'optional' or 'advanced' language feature, but the reality is that nearly all Java applications are multithreaded and these frameworks do not insulate you from the need to properly coordinate access to application state.",
+			"Whenever more than one thread accesses a given state variable, and one of them might write to it, they all must coordinate their access to it using synchronization. You should avoid the temptation to think that there are 'special' situations in which this rule does not apply.",
+			"A program that omits needed synchronization might appear to work, passing its tests and performing well for years, but it is still broken and may fail at any moment.", };
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		out.println("<html><head><title>Random quotes for everybody</title></head>");
 		out.println("<body><h2>Random quote</h2><p>");
 
-		String randomQuote = "test";
+		int randomIndex = 0;
+		String randomQuote = wordsOfWisdom[randomIndex];
 
 		out.println(randomQuote);
 		out.println("</p></body></html>");
