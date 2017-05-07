98624dca363e2c04cfc9c6e603726403cdf0ecef
se-vl <se-vl@users.noreply.github.com>
2017-05-07 17:42:08 +0200

    generate HTML response

--- quotes/src/servlets/QuoteServlet.java
+++ quotes/src/servlets/QuoteServlet.java
@@ -1,6 +1,7 @@
 package servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,7 +15,14 @@ public class QuoteServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>Random quotes for everybody</title></head>");
+		out.println("<body><h2>Random quote</h2><p>");
+
+		String randomQuote = "test";
+
+		out.println(randomQuote);
+		out.println("</p></body></html>");
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
